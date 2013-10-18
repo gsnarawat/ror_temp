080ccc19cb06b935bb8444bdcd2eb7527b169abd
Administrator <admin@local.host>
2013-10-18 12:38:16 +0530

    js

--- app/assets/javascripts/s3Slider.js
+++ app/assets/javascripts/s3Slider.js
@@ -1,12 +1,12 @@
 /* ------------------------------------------------------------------------
-	s3Slider
-	
-	Developped By: Boban Karišik -> http://www.serie3.info/
-        CSS Help: Mészáros Róbert -> http://www.perspectived.com/
-	Version: 1.0
-	
-	Copyright: Feel free to redistribute the script/modify it, as
-			   long as you leave my infos at the top.
+    s3Slider
+    
+    Developped By: Boban KariÅ¡ik -> http://www.serie3.info/
+        CSS Help: MÃ©szÃ¡ros RÃ³bert -> http://www.perspectived.com/
+    Version: 1.0
+    
+    Copyright: Feel free to redistribute the script/modify it, as
+               long as you leave my infos at the top.
 ------------------------------------------------------------------------- */
 
 
